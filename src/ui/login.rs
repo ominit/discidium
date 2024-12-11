b0fb9c99543ef1e24451daa36af4b8abdf9a390a
ominit <86736586+ominit@users.noreply.github.com>
2024-12-10 20:07:32 -0500

    swap to cushy

--- src/ui/login.rs
+++ src/ui/login.rs
@@ -1,29 +1,36 @@
-use ratatui::{
-    layout::{Constraint, Layout},
-    text::{Text, ToText},
-    widgets::{Block, Paragraph},
-    Frame,
+use std::sync::Arc;
+
+use cushy::{
+    value::{Destination, Dynamic, DynamicRead, Source},
+    widget::{MakeWidget, WidgetInstance},
+    widgets::input::InputValue,
 };
-use tui_textarea::TextArea;
+use parking_lot::Mutex;
 
-use super::{DiscidiumData, TUIApp};
+use super::DiscidiumData;
 
-pub fn login_ui(app: &mut TUIApp, frame: &mut Frame) {
-    let [area] = Layout::horizontal([Constraint::Length(20)])
-        .flex(ratatui::layout::Flex::Center)
-        .areas(frame.area());
-    let vertical = Layout::vertical([
-        Constraint::Length(1),
-        Constraint::Length(1),
-        Constraint::Length(1),
-        Constraint::Length(1),
-    ])
-    .flex(ratatui::layout::Flex::Center);
-    let [login_area, p_area, q_area, enter_area] = vertical.areas(area);
-    frame.render_widget(Text::raw("login").centered(), login_area);
-    frame.render_widget(Text::raw("p - paste token"), p_area);
-    frame.render_widget(Text::raw("q - quit"), q_area);
-    if app.text_edit.contains_key("login_token") {
-        frame.render_widget(Text::raw("enter - login with pasted token"), enter_area);
-    }
+pub fn login_ui(
+    data: Arc<Mutex<Option<DiscidiumData>>>,
+    is_logged_in: Dynamic<bool>,
+) -> WidgetInstance {
+    let token_input: Dynamic<String> = Dynamic::default();
+    token_input
+        .to_input()
+        .mask_symbol("•")
+        .placeholder("token")
+        .centered()
+        .and(
+            "login"
+                .into_button()
+                .on_click(move |_| {
+                    let test = DiscidiumData::from_token(token_input.read().to_string());
+                    if test.is_some() {
+                        is_logged_in.set(true);
+                        let _ = data.clone().lock().insert(test.unwrap());
+                    }
+                })
+                .centered(),
+        )
+        .into_rows()
+        .make_widget()
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,97 +2,46 @@ mod login;
 
 use std::{collections::BTreeMap, sync::Arc};
 
-use crossterm::event::{Event, KeyEvent, KeyModifiers};
+use cushy::{
+    value::{Destination, Dynamic, DynamicRead, IntoReader, Source, Switchable},
+    widget::{MakeWidget, WidgetInstance},
+    widgets::Label,
+    InputState, Run,
+};
 use keyring::Entry;
 use login::login_ui;
 use parking_lot::Mutex;
-use ratatui::{text::Text, DefaultTerminal, Frame};
 
-use crate::api::{client::Client, state::State, Connection};
+use crate::api::{
+    client::Client,
+    model::{ChannelId, ServerId},
+    state::State,
+    Connection,
+};
 
 pub fn create_ui() {
-    // DiscidiumData::delete_token();
-    let terminal = ratatui::init();
-    TUIApp::new().run(terminal);
-    ratatui::restore();
+    DiscidiumData::delete_token();
+    let data = Arc::new(Mutex::new(DiscidiumData::init()));
+    ui(data).run().unwrap();
 }
 
-pub(super) struct TUIApp {
-    pub data: Option<DiscidiumData>,
-    pub text_edit: BTreeMap<String, String>,
-}
-
-impl TUIApp {
-    fn new() -> Self {
-        Self {
-            data: DiscidiumData::init(),
-            text_edit: BTreeMap::new(),
-        }
-    }
-
-    fn run(mut self, mut terminal: DefaultTerminal) {
-        loop {
-            terminal
-                .draw(|frame| self.ui(frame))
-                .expect("unable to draw frame");
-            let event = crossterm::event::read().expect("failed to read event");
-            if self.data.is_none() {
-                if event
-                    == Event::Key(KeyEvent::new(
-                        crossterm::event::KeyCode::Char('q'),
-                        KeyModifiers::empty(),
-                    ))
-                {
-                    break;
-                }
-                if event
-                    == Event::Key(KeyEvent::new(
-                        crossterm::event::KeyCode::Char('p'),
-                        KeyModifiers::empty(),
-                    ))
-                {
-                    let token = arboard::Clipboard::new().unwrap().get_text();
-                    if token.is_ok() {
-                        self.text_edit
-                            .insert("login_token".to_owned(), token.unwrap());
-                    }
-                }
-                if event
-                    == Event::Key(KeyEvent::new(
-                        crossterm::event::KeyCode::Enter,
-                        KeyModifiers::empty(),
-                    ))
-                    && self.text_edit.contains_key("login_token")
-                {
-                    self.data =
-                        DiscidiumData::from_token(self.text_edit.remove("login_token").unwrap());
-                }
+fn ui(data: Arc<Mutex<Option<DiscidiumData>>>) -> WidgetInstance {
+    let is_logged_in = Dynamic::new(data.lock().is_some());
+    is_logged_in
+        .switcher(move |current: &bool, x| {
+            if !current {
+                return login_ui(data.clone(), x.clone());
             }
-            if event
-                == Event::Key(KeyEvent::new(
-                    crossterm::event::KeyCode::Char('q'),
-                    KeyModifiers::empty(),
-                ))
-            {
-                break;
-            }
-        }
-    }
-
-    fn ui(&mut self, frame: &mut Frame) {
-        if self.data.is_none() {
-            login_ui(self, frame);
-            return;
-        }
-        frame.render_widget(Text::raw("hi"), frame.area());
-    }
+            "a".make_widget()
+        })
+        .make_widget()
 }
 
 #[derive(Debug)]
 struct DiscidiumData {
-    client: Arc<Mutex<Client>>,
-    connection: Arc<Mutex<Connection>>,
-    state: Arc<Mutex<State>>,
+    client: Client,
+    connection: Connection,
+    state: State,
 }
 
 impl DiscidiumData {
@@ -102,13 +51,12 @@ impl DiscidiumData {
             return None;
         }
         let token = entry.unwrap().get_password().unwrap();
-        let client = Arc::new(Mutex::new(Client::from_user_token(token.into())));
-        let (_connection, ready) = match client.lock().connect() {
+        let client = Client::from_user_token(token.into());
+        let (connection, ready) = match client.connect() {
             Ok(a) => a,
             Err(err) => panic!("error connecting, Err: {:?}", err), // TODO if token doesnt work
         };
-        let connection = Arc::new(Mutex::new(_connection));
-        let state = Arc::new(Mutex::new(State::new(ready)));
+        let state = State::new(ready);
         Some(Self {
             client,
             connection,
